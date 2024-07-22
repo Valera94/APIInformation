aeb2ea5aff56ffe4ee1c126d4192093efc0b587d
DESKTOP-O3ICS2S\Valera <valerarslvskijj@yandex.ru>
2024-07-22 18:18:40 +0300

    Ready solution, assign with Wappi.

--- APIInformation/Source/APIInformation/Private/BFL/BFL_Plugin_APIInformation.cpp
+++ APIInformation/Source/APIInformation/Private/BFL/BFL_Plugin_APIInformation.cpp
@@ -2,9 +2,10 @@
 
 
 #include "BFL/BFL_Plugin_APIInformation.h"
+
+#include "DesktopPlatformModule.h"
 #include "Runtime/JsonUtilities/Public/JsonObjectConverter.h"
 #include "HttpModule.h"
-#include "Wappi/WappiApi.h"
 #include "Interfaces/IHttpResponse.h"
 #include "Windows/WindowsPlatformMisc.h"
 #include "Kismet/KismetSystemLibrary.h"
@@ -13,62 +14,47 @@
 #include "Misc/Base64.h"
 #include "Interfaces/IHttpRequest.h"
 
-void UBFL_Plugin_APIInformation::SendWhatsAppMessage(const FString SetMessage, const FString SetNumber, bool UseAsync)
+
+
+void UBFL_Plugin_APIInformation::SendMessage(const ETypeMessenger ChoiceTypeMessenger, bool isAsyncLogic,
+	const FString SetMessage, const FString SetNumber)
 {
-
-
+	if (ChoiceTypeMessenger == ETypeMessenger::EM_NONE) { return; }
 	if (!CheckPhoneToValid(SetNumber)) { return; }
 
-	FHttpModule* Http = &FHttpModule::Get();
-	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
-
-	UseAsync == true ?
-		Request->SetURL(TEXT("https://wappi.pro/api/async/message/send?profile_id=56afd854-76ae")) :
-		Request->SetURL(TEXT("https://wappi.pro/api/sync/message/send?profile_id=56afd854-76ae"));
-
-	Request->SetVerb("POST");
-	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
-	Request->SetHeader(TEXT("Authorization"), TEXT("a40e852c145d73a7c30f821a2d824247713bbea6"));
-
-
-	//FString Message = TEXT("Privet Konstantin pishu iz coda? testiruiu? vdrug nelsa otpravit ome kto ne v kontaktah =) !");
-	//FString Number = TEXT("79616348787");
+	FString l_URL;
+	URLSettings(ChoiceTypeMessenger,EA_Message, isAsyncLogic, l_URL);
 
 	FString JsonData = FString::Printf(TEXT("{ \"body\": \"%s\" , \"recipient\": \"%s\" }"), *SetMessage, *SetNumber);
 
-
-	Request->SetContentAsString(JsonData);
-	Request->ProcessRequest();
-
-
+	SetConfigRequest(JsonData, l_URL);
 }
 
-void UBFL_Plugin_APIInformation::SendFilePDF(const FString SetMessage, const FString SetNumber, const FString inFilePath)
+void UBFL_Plugin_APIInformation::SendFilePDF(const ETypeMessenger ChoiceTypeMessenger, 
+                                             bool isAsyncLogic, const FString SetMessage, const FString SetNumber, FString inFilePath)
 {
-
+	if (ChoiceTypeMessenger == ETypeMessenger::EM_NONE) { return; }
 	if (!CheckPhoneToValid(SetNumber)) { return; }
 
-	FString Base64String;
-	if (ConvertToBase64(inFilePath, Base64String)) { return; }
+	if(inFilePath.IsEmpty())
+	{
+		inFilePath = Screenshot(); // We can change this to other logic;
+	}
 
+	FString l_URL;
+	URLSettings(ChoiceTypeMessenger, EA_Image, isAsyncLogic, l_URL);
 
-	FHttpModule* Http = &FHttpModule::Get();
-	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	FString l_Base64String;
+	if (!ConvertToBase64(inFilePath, l_Base64String)) { return; }
 
-	Request->SetURL(TEXT("https://wappi.pro/api/sync/message/send?profile_id=56afd854-76ae"));
+	//Data with information to WappiApi
+	FString JsonData = FString::Printf(
+		TEXT("{  \"recipient\": \"%s\",	\"caption\": \"%s\",	\"b64_file\": \"%s\" }"),
+		*SetNumber, *SetMessage, *l_Base64String);
 
-	Request->SetVerb("POST");
-	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
-	Request->SetHeader(TEXT("Authorization"), TEXT("a40e852c145d73a7c30f821a2d824247713bbea6"));
-
-	FString JsonData = FString::Printf(TEXT("{  \"recipient\": \"%s\",\"caption\": \"%s\",\"b64_file\": \"%s\" }"),
-		*SetNumber, *SetMessage,*Base64String);
-
-	Request->SetContentAsString(JsonData);
-	Request->ProcessRequest();
-
-	
+	SetConfigRequest(JsonData, l_URL);
 }
+ 
 
 bool UBFL_Plugin_APIInformation::CheckPhoneToValid(const FString SetNumber)
 {
@@ -91,7 +77,16 @@ bool UBFL_Plugin_APIInformation::ConvertToBase64(const FString& inFilePath, FStr
 	if (FFileHelper::LoadFileToArray(FileData, *inFilePath))
 	{
 		Base64String = FBase64::Encode(FileData);
-		return true;
+
+		if (Base64String.IsEmpty())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Empty FBase64::Encode: Base64String"));
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 	}
 	else
 	{
@@ -99,3 +94,79 @@ bool UBFL_Plugin_APIInformation::ConvertToBase64(const FString& inFilePath, FStr
 		return false;
 	}
 }
+
+FString UBFL_Plugin_APIInformation::Screenshot()
+{
+	// Change to other settings
+
+	TArray<FString> selectedFiles;
+	//GScreenshotResolutionX = 400;
+	//GScreenshotResolutionY = 400;
+
+	FString fileName = FPaths::Combine(*FPaths::ProjectSavedDir(), *FString("Screenshots\\screenshot.png"));
+
+	FScreenshotRequest::RequestScreenshot(fileName, false, false);
+
+	 return	fileName;
+
+}
+
+
+void UBFL_Plugin_APIInformation::URLSettings(const ETypeMessenger inChoiceTypeMessenger,const ETypeAction inTypeAction, bool inAsyncTypeURL,
+	FString& URL)
+{
+
+	FString l_TypeMessengerURL;
+	FString l_ID_TypeMessengerURL;
+
+	//Set need config for url 
+	switch (inChoiceTypeMessenger)
+	{
+	case ETypeMessenger::EM_Whatsapp:
+		l_TypeMessengerURL = TEXT("api/");
+		l_ID_TypeMessengerURL = FString(TOSTRING(ID_PROFILE_WAPPI_WHATSAPP));
+
+		break;
+	case ETypeMessenger::EM_Telegram:
+		l_TypeMessengerURL = TEXT("tapi/");
+		l_ID_TypeMessengerURL = FString(TOSTRING(ID_PROFILE_WAPPI_TELEGRAM));
+
+		break;
+	}
+
+	FString l_AsyncTypeURL = inAsyncTypeURL ? "async/" : "sync/";
+
+
+	FString lTypeAction;
+	switch (inTypeAction)
+	{
+	case ETypeAction::EA_Image:
+		lTypeAction = ("img/");
+			break;
+	}
+
+	URL = FString("https://wappi.pro/")
+		.Append(l_TypeMessengerURL)
+		.Append(l_AsyncTypeURL)
+		.Append("message/")
+		.Append(lTypeAction)
+		.Append("send?profile_id=")
+		.Append(l_ID_TypeMessengerURL);
+}
+
+void UBFL_Plugin_APIInformation::SetConfigRequest(FString inJsonData, FString inURL)
+{
+
+	FHttpModule* Http = &FHttpModule::Get();
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+
+	Request->SetURL(inURL);
+	Request->SetVerb("POST");
+	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+	Request->SetHeader(TEXT("Authorization"), TOSTRING(TOKEN_API_WAPPI));
+
+	Request->SetContentAsString(inJsonData);
+	Request->ProcessRequest();
+
+}
+  
--- APIInformation/Source/APIInformation/Public/BFL/BFL_Plugin_APIInformation.h
+++ APIInformation/Source/APIInformation/Public/BFL/BFL_Plugin_APIInformation.h
@@ -6,29 +6,55 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "BFL_Plugin_APIInformation.generated.h"
 
+//You Need Set Define variable from Wappi.com  
+#define TOKEN_API_WAPPI a40e852c145d73a7c30f821a2d824247713bbea6
+#define ID_PROFILE_WAPPI_TELEGRAM 6d13282a-dc14
+#define ID_PROFILE_WAPPI_WHATSAPP 56afd854-76ae
+
+// Don't touch
+#define STRINGIFY(s) #s
+#define TOSTRING(s) STRINGIFY(s)
+
+
+UENUM(BlueprintType)
+enum ETypeMessenger
+{
+	EM_NONE		UMETA(DisplayName = "NONE"),
+	EM_Whatsapp UMETA(DisplayName = "Whatsapp"),
+	EM_Telegram UMETA(DisplayName = "Telegram"),
+};
+
+UENUM(BlueprintType)
+enum ETypeAction
+{
+	EA_NONE		UMETA(DisplayName = "NONE"),
+	EA_Message	UMETA(DisplayName = "Message"),
+	EA_Image	UMETA(DisplayName = "Image"),
+};
+
 UCLASS()
 class APIINFORMATION_API UBFL_Plugin_APIInformation : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
-	//	Todo NeedSetThisVariableToOtherPlace mb ProjectSettings
-	//	Todo Id and Token.
-	//	У каждого заказчика будет свой токен и id поэтому, он должен устанавливаться в отдельном файле,
-	//	где есть вся атрибутика определенного заказчика.
+	UFUNCTION(BlueprintCallable, Category = "Plugin_APIInformation")
+	static void SendMessage(const ETypeMessenger ChoiceTypeMessenger, bool isAsyncLogic, const FString SetMessage, const FString SetNumber);
 
-	UFUNCTION(BlueprintCallable, Category = "Plugin_APIInformation|WhatsApp")
-	static void  SendWhatsAppMessage(const FString SetMessage, const FString SetNumber, bool UseAsync = false);
-
-	UFUNCTION(BlueprintCallable, Category = "Plugin_APIInformation|WhatsApp")
-	static void SendFilePDF(const FString SetMessage, const FString SetNumber, const FString inFilePath);
-
+	UFUNCTION(BlueprintCallable, Category = "Plugin_APIInformation")
+	static void SendFilePDF(const ETypeMessenger ChoiceTypeMessenger, bool isAsyncLogic, const FString SetMessage, const FString SetNumber, FString inFilePath);
 
 	UFUNCTION(BlueprintCallable, Category = "Plugin_APIInformation")
 	static bool CheckPhoneToValid(const FString SetNumber);
 
 
+	//Configurate
+
 	//bool true - success, false - fail load file
 	static bool ConvertToBase64(const FString& inFilePath, FString& Base64String);
-
+	static FString Screenshot();
+	static void URLSettings(const ETypeMessenger inChoiceTypeMessenger, const ETypeAction inTypeAction, bool inAsyncTypeURL, FString& URL);
+	static void SetConfigRequest(FString inJsonData, FString inURL);
 
 };
+
+
